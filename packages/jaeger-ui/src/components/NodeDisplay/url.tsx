3d07f763e75a7f8f94add815118b5bd82c35cc4c
mtn-2020 <b17t2105c@edu.tottori-u.ac.jp>
2020-12-21 16:48:57 +0900

    test

--- packages/jaeger-ui/src/components/NodeDisplay/url.tsx
+++ packages/jaeger-ui/src/components/NodeDisplay/url.tsx
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import memoizeOne from 'memoize-one';
 import { matchPath } from 'react-router-dom';
 
 import prefixUrl from '../../utils/prefix-url';
