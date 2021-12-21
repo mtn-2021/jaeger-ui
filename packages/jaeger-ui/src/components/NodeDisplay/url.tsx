448536964694874afdf77060e3aea488c0a4cc20
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2021-12-21 18:00:25 +0900

    tmp

--- packages/jaeger-ui/src/components/NodeDisplay/url.tsx
+++ packages/jaeger-ui/src/components/NodeDisplay/url.tsx
@@ -12,19 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { matchPath } from 'react-router-dom';
-
-import prefixUrl from '../../utils/prefix-url';
-
-export const ROUTE_PATH = prefixUrl('/nodes');
-
-const ROUTE_MATCHER = { path: ROUTE_PATH, strict: true, exact: true };
-
-export function matches(path: string) {
-  return Boolean(matchPath(path, ROUTE_MATCHER));
-}
-
-export function getUrl() {
-  return prefixUrl('/nodes');
-  // searchのは検索の設定用引数
-}
+// import { matchPath } from 'react-router-dom';
+//
+// import prefixUrl from '../../utils/prefix-url';
+//
+// export const ROUTE_PATH = prefixUrl('/nodes');
+//
+// const ROUTE_MATCHER = { path: ROUTE_PATH, strict: true, exact: true };
+//
+// export function matches(path: string) {
+//   return Boolean(matchPath(path, ROUTE_MATCHER));
+// }
+//
+// export function getUrl() {
+//   return prefixUrl('/nodes');
+//   // searchのは検索の設定用引数
+// }
