50372334e8850a1fff1ef25be1cf219dd2618322
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2021-12-25 03:12:55 +0900

    a

--- packages/jaeger-ui/src/components/NodeDisplay/SearchResults/ResultGraph.js
+++ packages/jaeger-ui/src/components/NodeDisplay/SearchResults/ResultGraph.js
@@ -15,7 +15,7 @@
 import React from 'react';
 import dimensions from 'react-dimensions';
 import { Bar } from 'react-chartjs-2';
-import moment from 'moment';
+// import moment from 'moment';
 import PropTypes from 'prop-types';
 import { ONE_MILLISECOND } from '../../../utils/date';
 
@@ -29,13 +29,13 @@ function getUintTime(time) {
   return parseInt(time, 10) * units[unit] * 1000 * 1000;
 }
 
-const colors = {
-  0: 'rgba(30, 144, 255, 1)',
-  1: 'rgba(144, 101, 224, 1)',
-  2: 'rgba(206, 101, 224, 1)',
-  3: 'rgba(160, 44, 47, 1)',
-  4: 'rgba(210, 83, 44, 1)',
-};
+// const colors = {
+//   0: 'rgba(30, 144, 255, 1)',
+//   1: 'rgba(144, 101, 224, 1)',
+//   2: 'rgba(206, 101, 224, 1)',
+//   3: 'rgba(160, 44, 47, 1)',
+//   4: 'rgba(210, 83, 44, 1)',
+// };
 
 const UnitKeeper = {
   m: { format: '~hh:mm a', half: '30s', full: 'minute' },
@@ -43,188 +43,188 @@ const UnitKeeper = {
   d: { format: '~MM/DD', underUnit: 'h', half: '12h', full: 'day' },
 };
 
-function getIntervalTime(lookback) {
-  const unit = lookback.substr(-1);
-  const vol = parseInt(lookback, 10);
-  const interval = Math.floor(vol / 10);
-  if (interval !== 0) {
-    return {
-      time: getUintTime(interval + unit),
-      unit,
-      interval: interval + unit,
-    };
-  }
-  if (vol > 4) {
-    return {
-      time: getUintTime(UnitKeeper[unit].half),
-      unit: UnitKeeper[unit].underUnit,
-      interval: UnitKeeper[unit].half,
-    };
-  }
-  if (unit === 'd') {
-    return {
-      time: getUintTime(vol * 2 + UnitKeeper[unit].underUnit),
-      unit: UnitKeeper[unit].underUnit,
-      interval: vol * 2 + UnitKeeper[unit].underUnit,
-    };
-  }
-  if (unit === 'h') {
-    return {
-      time: getUintTime(vol * 5 + UnitKeeper[unit].underUnit),
-      unit: UnitKeeper[unit].underUnit,
-      interval: vol * 5 + UnitKeeper[unit].underUnit,
-    };
-  }
-  return {
-    time: getUintTime(1 + unit),
-    unit,
-    interval: 1 + unit,
-  };
-}
+// function getIntervalTime(lookback) {
+//   const unit = lookback.substr(-1);
+//   const vol = parseInt(lookback, 10);
+//   const interval = Math.floor(vol / 10);
+//   if (interval !== 0) {
+//     return {
+//       time: getUintTime(interval + unit),
+//       unit,
+//       interval: interval + unit,
+//     };
+//   }
+//   if (vol > 4) {
+//     return {
+//       time: getUintTime(UnitKeeper[unit].half),
+//       unit: UnitKeeper[unit].underUnit,
+//       interval: UnitKeeper[unit].half,
+//     };
+//   }
+//   if (unit === 'd') {
+//     return {
+//       time: getUintTime(vol * 2 + UnitKeeper[unit].underUnit),
+//       unit: UnitKeeper[unit].underUnit,
+//       interval: vol * 2 + UnitKeeper[unit].underUnit,
+//     };
+//   }
+//   if (unit === 'h') {
+//     return {
+//       time: getUintTime(vol * 5 + UnitKeeper[unit].underUnit),
+//       unit: UnitKeeper[unit].underUnit,
+//       interval: vol * 5 + UnitKeeper[unit].underUnit,
+//     };
+//   }
+//   return {
+//     time: getUintTime(1 + unit),
+//     unit,
+//     interval: 1 + unit,
+//   };
+// }
 
-function getLabelsAndData(lookback, start, request, operationNames, graphMenu) {
-  const lookbackTime = getUintTime(lookback);
-  const interval = getIntervalTime(lookback);
-  const format = UnitKeeper[interval.unit].format;
-  const intervalTime = interval.time;
-  const length = lookbackTime / intervalTime;
-  let requestLength = request.length - 1;
-  let time = start;
-  const labels = [];
-  const data = [];
-  const dataCount = [];
+// function getLabelsAndData(lookback, start, request, operationNames, graphMenu) {
+//   const lookbackTime = getUintTime(lookback);
+//   const interval = getIntervalTime(lookback);
+//   const format = UnitKeeper[interval.unit].format;
+//   const intervalTime = interval.time;
+//   const length = lookbackTime / intervalTime;
+//   let requestLength = request.length - 1;
+//   let time = start;
+//   const labels = [];
+//   const data = [];
+//   const dataCount = [];
+//
+//   for (let i = 0; i <= length - 1; i++) {
+//     const dataPart = [];
+//     operationNames.forEach(operationName => {
+//       dataPart[operationName] = 0;
+//     });
+//     let count = 0;
+//
+//     for (let l = requestLength; l >= 0; l--) {
+//       if (request[l].startTime >= time && request[l].startTime < time + intervalTime) {
+//         if (graphMenu === 'allRequest') {
+//           count++;
+//           dataPart[request[l].operationName] += 1;
+//         } else if (graphMenu === 'failedRequest' && request[l].failed !== null) {
+//           count++;
+//           dataPart[request[l].operationName] += 1;
+//         } else if (graphMenu === 'successRequest' && request[l].failed === null) {
+//           count++;
+//           dataPart[request[l].operationName] += 1;
+//         }
+//       } else {
+//         requestLength = l;
+//         break;
+//       }
+//     }
+//
+//     data.push(count);
+//     for (let op = 0; op < operationNames.length; op++) {
+//       const tmpCount = [].concat(dataCount[operationNames[op]]);
+//       tmpCount.push(dataPart[operationNames[op]]);
+//       dataCount[operationNames[op]] = tmpCount;
+//     }
+//
+//     time += intervalTime;
+//     const label = moment(time / ONE_MILLISECOND)
+//       .format(format)
+//       .split(' ');
+//     labels.push(label);
+//   }
+//
+//   let datasets = operationNames.map((operationName, index) => ({
+//     type: 'bar',
+//     data: [].concat(dataCount[operationName]).filter(v => !(v === undefined)),
+//     label: operationName,
+//     backgroundColor: colors[index],
+//     yAxisID: 'y-bar',
+//     xAxisID: 'x-bar',
+//   }));
+//   if (!(datasets && datasets.length > 0))
+//     datasets = [
+//       {
+//         type: 'bar',
+//         data,
+//         label: 'Number of request',
+//         yAxisID: 'y-bar',
+//         xAxisID: 'x-bar',
+//       },
+//     ];
+//
+//   const max = data.reduce((a, b) => Math.max(a, b));
+//   const maxData = 5 - (max % 5) + max;
+//
+//   return { labels, datasets, maxData, interval };
+// }
 
-  for (let i = 0; i <= length - 1; i++) {
-    const dataPart = [];
-    operationNames.forEach(operationName => {
-      dataPart[operationName] = 0;
-    });
-    let count = 0;
-
-    for (let l = requestLength; l >= 0; l--) {
-      if (request[l].startTime >= time && request[l].startTime < time + intervalTime) {
-        if (graphMenu === 'allRequest') {
-          count++;
-          dataPart[request[l].operationName] += 1;
-        } else if (graphMenu === 'failedRequest' && request[l].failed !== null) {
-          count++;
-          dataPart[request[l].operationName] += 1;
-        } else if (graphMenu === 'successRequest' && request[l].failed === null) {
-          count++;
-          dataPart[request[l].operationName] += 1;
-        }
-      } else {
-        requestLength = l;
-        break;
-      }
-    }
-
-    data.push(count);
-    for (let op = 0; op < operationNames.length; op++) {
-      const tmpCount = [].concat(dataCount[operationNames[op]]);
-      tmpCount.push(dataPart[operationNames[op]]);
-      dataCount[operationNames[op]] = tmpCount;
-    }
-
-    time += intervalTime;
-    const label = moment(time / ONE_MILLISECOND)
-      .format(format)
-      .split(' ');
-    labels.push(label);
-  }
-
-  let datasets = operationNames.map((operationName, index) => ({
-    type: 'bar',
-    data: [].concat(dataCount[operationName]).filter(v => !(v === undefined)),
-    label: operationName,
-    backgroundColor: colors[index],
-    yAxisID: 'y-bar',
-    xAxisID: 'x-bar',
-  }));
-  if (!(datasets && datasets.length > 0))
-    datasets = [
-      {
-        type: 'bar',
-        data,
-        label: 'Number of request',
-        yAxisID: 'y-bar',
-        xAxisID: 'x-bar',
-      },
-    ];
-
-  const max = data.reduce((a, b) => Math.max(a, b));
-  const maxData = 5 - (max % 5) + max;
-
-  return { labels, datasets, maxData, interval };
-}
-
-function getStatusPlot(status, yPlop) {
-  const statusData = [];
-  const missingData = [];
-  // statusData.push(
-  //     {
-  //       operationName: "test",
-  //       x: 164035633771,
-  //       y: 0.5,
-  //     });
-  // missingData.push(
-  //     {
-  //       operationName: "test2",
-  //       x: 164035640000,
-  //       y: 1,
-  //     });
-  const statusFlag = yPlop === 'status';
-  let maxData = -Infinity;
-
-  status.forEach(stPart => {
-    if (stPart.isOn) {
-      const index = [].concat(stPart.keys).indexOf(yPlop);
-      if (index >= 0) {
-        const value = statusFlag ? 1 : parseInt(stPart.values[index], 10);
-        statusData.push({
-          operationName: stPart.operationName,
-          x: stPart.timestamp / ONE_MILLISECOND,
-          y: value,
-        });
-        if (maxData < value) maxData = value;
-      }
-    } else {
-      missingData.push({
-        operationName: stPart.operationName,
-        x: stPart.timestamp / ONE_MILLISECOND,
-        y: 0,
-      });
-    }
-  });
-
-  const datasets = [
-    {
-      type: 'scatter',
-      data: statusData,
-      label: yPlop,
-      backgroundColor: `rgba(117, 219, 219,1)`,
-      yAxisID: 'y-scat',
-      xAxisID: 'x-scat',
-    },
-    {
-      type: 'scatter',
-      data: missingData,
-      label: 'disConnect',
-      backgroundColor: `rgba(255, 15, 43, 1)`,
-      yAxisID: 'y-scat',
-      xAxisID: 'x-scat',
-    },
-  ];
-
-  let i;
-  for (i = 0; maxData > 100; i++) {
-    maxData /= 10;
-  }
-
-  maxData = Math.ceil(maxData) * 10 ** i;
-  if (maxData === -Infinity) maxData = 1;
-  return { datasets, maxData };
-}
+// function getStatusPlot(status, yPlop) {
+//   const statusData = [];
+//   const missingData = [];
+//   // statusData.push(
+//   //     {
+//   //       operationName: "test",
+//   //       x: 164035633771,
+//   //       y: 0.5,
+//   //     });
+//   // missingData.push(
+//   //     {
+//   //       operationName: "test2",
+//   //       x: 164035640000,
+//   //       y: 1,
+//   //     });
+//   const statusFlag = yPlop === 'status';
+//   let maxData = -Infinity;
+//
+//   status.forEach(stPart => {
+//     if (stPart.isOn) {
+//       const index = [].concat(stPart.keys).indexOf(yPlop);
+//       if (index >= 0) {
+//         const value = statusFlag ? 1 : parseInt(stPart.values[index], 10);
+//         statusData.push({
+//           operationName: stPart.operationName,
+//           x: stPart.timestamp / ONE_MILLISECOND,
+//           y: value,
+//         });
+//         if (maxData < value) maxData = value;
+//       }
+//     } else {
+//       missingData.push({
+//         operationName: stPart.operationName,
+//         x: stPart.timestamp / ONE_MILLISECOND,
+//         y: 0,
+//       });
+//     }
+//   });
+//
+//   const datasets = [
+//     {
+//       type: 'scatter',
+//       data: statusData,
+//       label: yPlop,
+//       backgroundColor: `rgba(117, 219, 219,1)`,
+//       yAxisID: 'y-scat',
+//       xAxisID: 'x-scat',
+//     },
+//     {
+//       type: 'scatter',
+//       data: missingData,
+//       label: 'disConnect',
+//       backgroundColor: `rgba(255, 15, 43, 1)`,
+//       yAxisID: 'y-scat',
+//       xAxisID: 'x-scat',
+//     },
+//   ];
+//
+//   let i;
+//   for (i = 0; maxData > 100; i++) {
+//     maxData /= 10;
+//   }
+//
+//   maxData = Math.ceil(maxData) * 10 ** i;
+//   if (maxData === -Infinity) maxData = 1;
+//   return { datasets, maxData };
+// }
 
 function ResultGraphImpl(props) {
   const { request, status, lookback: lb, start, operationNames, statusKey, graphMenu } = props;
@@ -236,17 +236,18 @@ function ResultGraphImpl(props) {
   let minData;
   let maxDataB;
   let maxDataS;
+  console.log({lb,graphMenu});
   if ((status && status.length > 0) || (request && request.length > 0)) {
-    lookback = lb;
-    const structure = getLabelsAndData(lookback, start, request, operationNames, graphMenu);
-    const statusData = getStatusPlot(status, statusKey);
-    labels = structure.labels;
-    datasets = [].concat(statusData.datasets).concat(structure.datasets);
-    xLabel = `Last ${parseInt(lookback, 10)} ${UnitKeeper[lookback.substr(-1)].full}`;
-    intervalUnit = structure.interval;
-    minData = 0;
-    maxDataB = structure.maxData;
-    maxDataS = statusData.maxData;
+    // lookback = lb;
+    // const structure = getLabelsAndData(lookback, start, request, operationNames, graphMenu);
+    // const statusData = getStatusPlot(status, statusKey);
+    // labels = structure.labels;
+    // datasets = [].concat(statusData.datasets).concat(structure.datasets);
+    // xLabel = `Last ${parseInt(lookback, 10)} ${UnitKeeper[lookback.substr(-1)].full}`;
+    // intervalUnit = structure.interval;
+    // minData = 0;
+    // maxDataB = structure.maxData;
+    // maxDataS = statusData.maxData;
   } else {
     lookback = '1h';
     // const structure = getLabelsAndData(
