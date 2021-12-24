f3b6030c9a078f70fa417229641f46e68788b8f3
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2021-12-25 04:03:51 +0900

    a

--- packages/jaeger-ui/src/components/NodeDisplay/SearchResults/ResultGraph.js
+++ packages/jaeger-ui/src/components/NodeDisplay/SearchResults/ResultGraph.js
@@ -14,10 +14,10 @@
 
 import React from 'react';
 import dimensions from 'react-dimensions';
-// import { Bar } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 // import moment from 'moment';
 import PropTypes from 'prop-types';
-//import { ONE_MILLISECOND } from '../../../utils/date';
+// import { ONE_MILLISECOND } from '../../../utils/date';
 
 // function getUintTime(time) {
 //   const unit = time.substr(-1);
