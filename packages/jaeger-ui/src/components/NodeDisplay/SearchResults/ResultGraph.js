ef3cf2a94944faea5f1de332eed5e9f2dc49d88b
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2021-12-25 02:46:00 +0900

    a

--- packages/jaeger-ui/src/components/NodeDisplay/SearchResults/ResultGraph.js
+++ packages/jaeger-ui/src/components/NodeDisplay/SearchResults/ResultGraph.js
@@ -257,7 +257,7 @@ function ResultGraphImpl(props) {
     //   graphMenu
     // );
     const structure = {
-      interval: 'h',
+      interval: {unit: 'h'},
       labels: ["1","2","3","4","5"],
       datasets: [
           {
